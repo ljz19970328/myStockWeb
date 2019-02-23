d64ce97c762d7c6c1d3df9b8f9a4560cc2520928
ljz19970328 <1341652498@qq.com>
2019-02-23 19:59:43 +0800

    news修改

--- myStockApp/static/js/dealEchart.js
+++ myStockApp/static/js/dealEchart.js
@@ -286,65 +286,6 @@ function show_k_line(data,type) {
     myChart.setOption(option);
 }
 
-function show_calendar(data){
-var dateList = data.dateList
-
-var Data = [];
-for (var i = 0; i < dateList.length; i++) {
-    Data.push([
-        dateList[i],
-        1
-    ]);
-}
-option = {
-    visualMap: {
-                show: false,
-                min: 0,
-                max: 300,
-                calculable: true,
-                seriesIndex: [2],
-                orient: 'horizontal',
-                left: 'center',
-                bottom: 0,
-                inRange: {
-                    color: ['#fff', '#fff']
-                }
-            },
-    calendar: [{
-        left: 'center',
-        top: 'middle',
-        cellSize: [40, 40],
-        yearLabel: {show: false},
-        orient: 'vertical',
-        dayLabel: {
-            firstDay: 1,
-            nameMap: 'cn'
-        },
-        range: '2019-02'
-    }],
-
-    series: [{
-        type: 'scatter',
-        coordinateSystem: 'calendar',
-        symbolSize: 20,
-        label: {
-            normal: {
-                show: true,
-                formatter: function (params) {
-                    var d = echarts.number.parseDate(params.value[0]);
-                    return d.getDate();
-                },
-                textStyle: {
-                    color: '#000'
-                }
-            }
-        },
-        data: Data
-    }
-]
-};
-}
-
 
 
 function show_k_line_yuan(data,type) {
@@ -547,4 +488,63 @@ function show_k_line_yuan(data,type) {
     };
     // 使用刚指定的配置项和数据显示图表
     myChart.setOption(option);
+}
+
+function show_calendar(data){
+var dateList = data.dateList
+
+var Data = [];
+for (var i = 0; i < dateList.length; i++) {
+    Data.push([
+        dateList[i],
+        1
+    ]);
+}
+option = {
+    visualMap: {
+                show: false,
+                min: 0,
+                max: 300,
+                calculable: true,
+                seriesIndex: [2],
+                orient: 'horizontal',
+                left: 'center',
+                bottom: 0,
+                inRange: {
+                    color: ['#fff', '#fff']
+                }
+            },
+    calendar: [{
+        left: 'center',
+        top: 'middle',
+        cellSize: [40, 40],
+        yearLabel: {show: false},
+        orient: 'vertical',
+        dayLabel: {
+            firstDay: 1,
+            nameMap: 'cn'
+        },
+        range: '2019-02'
+    }],
+
+    series: [{
+        type: 'scatter',
+        coordinateSystem: 'calendar',
+        symbolSize: 20,
+        label: {
+            normal: {
+                show: true,
+                formatter: function (params) {
+                    var d = echarts.number.parseDate(params.value[0]);
+                    return d.getDate();
+                },
+                textStyle: {
+                    color: '#000'
+                }
+            }
+        },
+        data: Data
+    }
+]
+};
 }
